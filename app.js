97f6d14731ecd4118c097d67e388697ab128189d
bazilakbar <bazilakbar4@gmail.com>
2024-04-04 12:40:27 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+let age = 18
+
+alert ("I am " +age+ " years old")
+
+let timesToVisit = 14
+
+alert ("You have this site " +timesToVisit+ " times")
+
+
+let visitorName = "Alexander"
+let productTitle = "T-Shirts"
+let quantity = 5
+
+document.write (visitorName + " ordered " + quantity + productTitle + " on ABC Store ")
